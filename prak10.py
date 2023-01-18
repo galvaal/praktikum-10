75b02fbaf2b7a803096911cce8622972571ca684
galvaal <115516730+galvaal@users.noreply.github.com>
2023-01-18 16:37:06 +0700

    Add files via upload

---
+++ prak10.py
@@ -0,0 +1,8 @@
+txt = "Hello World"
+print(len(txt))                   # Len untuk menghitung jumlah karakter yang ada di dalam sebuah variable
+print(txt[10])                    # Akan mengeluarkan karakter ke 10
+print(txt[2:5])                   # Akan mengeluarkan karakter 2 s/d 5
+print(txt.replace(" ", ""))       # Mengganti karakter white space menjadi tanpa white space
+print(txt.upper())                # Membuat semua karakter menjadi huruf kapital
+print(txt.lower())                # Membuat semua karakter menjadi huruf kecil
+print(txt.replace("H", "J"))      # Mengubah huruf, dari huruf "H" menjadi huruf "J"
---
+++ prak10lat2.py
@@ -0,0 +1,3 @@
+umur = 24
+txt = "Hello, nama saya Galva Al, dan umur saya adalah {} tahun"
+print(txt.format(umur))
